5331dec5d7c126e041c69d47e81488e58ab29bc5
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2019-11-05 19:26:16 +0100

    sales table (mvp)

--- migrations/20191105162141_cars.js
+++ migrations/20191105162141_cars.js
@@ -21,8 +21,21 @@ exports.up = function(knex) {
             cars
                 .string('status')
         })
+        .createTable('sales', function(sales){
+            sales.increments()
+            sales.boolean('sold').notNullable().defaultTo(1)
+    
+            sales
+                .integer('car_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('cars')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE');
+        })
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('cars')
+  return knex.schema.dropTableIfExists('cars').dropTableIfExists('sales')
 };
